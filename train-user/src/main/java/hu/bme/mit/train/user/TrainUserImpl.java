e4d1b512c5ef41a053a4809635b679a7b8f661b1
Laki <laki@wanari.com>
2021-03-29 12:25:09 +0200

    Implement alarming function

--- train-user/src/main/java/hu/bme/mit/train/user/TrainUserImpl.java
+++ train-user/src/main/java/hu/bme/mit/train/user/TrainUserImpl.java
@@ -7,11 +7,21 @@ public class TrainUserImpl implements TrainUser {
 
 	private TrainController controller;
 	private int joystickPosition;
+	private boolean alarmState;
 
 	public TrainUserImpl(TrainController controller) {
 		this.controller = controller;
 	}
 
+	@Override
+	public boolean getAlarmState() {
+		return alarmState;
+	}
+
+	@Override
+	public void setAlarmState(boolean alarmState) {
+		this.alarmState = alarmState;
+	}
 	@Override
 	public boolean getAlarmFlag() {
 		return false;
